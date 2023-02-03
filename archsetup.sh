39be6e4a513fa71c5e37b235480b78eb0532d7a5
mathbike <mathbike8@gmail.com>
2023-02-03 03:51:56 -0500

    d

--- archsetup.sh
+++ archsetup.sh
@@ -11,7 +11,15 @@ packages_base() {
 
 packages_extra() {
 	sudo pacman -S --noconfirm \
-		gnupg pass passmenu zathura zathura-pdf-mupdf nodejs npm gimp inkscape texlive-most
+		gnupg pass passmenu zathura zathura-pdf-mupdf nodejs npm gimp inkscape texlive-most freecad
+}
+
+firewall() {
+	sudo systemctl enable ufw.service
+	sudo ufw default deny incoming
+	sudo ufw default allow outgoing
+	sudo ufw logging off
+	sudo ufw enable
 }
 
 dotfiles() {
@@ -19,5 +27,27 @@ dotfiles() {
     alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
     config checkout 2>&1 | grep -E "\s+\." | awk {'print $1'} | xargs -I{} rm {}
     config checkout
+	config config --local status.showUntrackedFiles no
+}
+
+configuration() {
+	git clone https://github.com/mathbike/suckless.git $HOME/.config/suckless
+	cd $HOME/.config/suckless/st && sudo make install
+	cd $HOME/.config/suckless/dwm && sudo make install
+	cd $HOME/.config/suckless/dwm/dwmblocks && sudo make install
+	cd $HOME/.config/suckless/dmenu && sudo make install && cd
+	ln -s $HOME/.config/suckless/dwm/dwmstatusbar/sb-battery.sh /usr/local/bin
+	ln -s $HOME/.config/suckless/dwm/dwmstatusbar/sb-brightness.sh /usr/local/bin
+	ln -s $HOME/.config/suckless/dwm/dwmstatusbar/sb-date.sh /usr/local/bin
+	ln -s $HOME/.config/suckless/dwm/dwmstatusbar/sb-volume.sh /usr/local/bin
+}
+
+other() {
+	# yay
+	git clone https://aur.archlinux.org/yay-git.git $HOME/.config/yay
+	cd $HOME/.config/yay && makepkg -si --noconfirm
+	# brave
+	yay -S brave-bin --noconfirm
+
 }
 
