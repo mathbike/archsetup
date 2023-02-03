4a72f7291af4006be02edbff1caca5a15f0cb855
mathbike <mathbike8@gmail.com>
2023-02-03 03:38:36 -0500

    d

--- archsetup.sh
+++ archsetup.sh
@@ -4,16 +4,15 @@
 
 packages() {
 	sudo pacman -S --noconfirm \
-		base-devel linux-headers linux-firmware \
-		xorg-server xorg-xinit xorg-xrandr xorg-xsetroot libxft libxinerama xclip xorg-xinput \
-		alsa-utils pulseaudio bashtop \
-		ttf-jetbrains-mono man-db man-pages git github-cli \
-		ufw gnupg pass passmenu python-pip xdotool \
-		zathura zathura-pdf-mupdf brightnessctl nodejs npm gimp inkscape tlp \
-		texlive-most coreutils
+		base-devel linux-headers linux-firmware ufw python-pip brightnessctl code \
+		xorg-server xorg-xinit xorg-xrandr xorg-xsetroot libxft libxinerama xclip xorg-xinput xdotool \
+		coreutils alsa-utils pulseaudio bashtop ttf-jetbrains-mono man-db man-pages git github-cli tlp
 }
 
-
+packages() {
+	sudo pacman -S --noconfirm \
+		gnupg pass passmenu zathura zathura-pdf-mupdf nodejs npm gimp inkscape texlive-most
+}
 
 
 dotfiles() {
